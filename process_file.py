3ea991df9ac680729d076c07c885b39e7adcda16
Alejandro Rico <arico@uc.cl>
2022-03-28 09:57:09 -0300

    refactor(process_file.py): created module to process file

---
+++ process_file.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+def users():
+    raw_tweets = get_raw_tweets()
+    users = pd.json_normalize(raw_tweets['user'])
+    users.rename(columns={'id':'userId', 'url':'profileUrl'}, inplace=True)
+    users = pd.DataFrame(users)
+    users.drop_duplicates(subset=['userId'], inplace=True)
+    return users
+
+def tweets():
+    raw_tweets = get_raw_tweets()
+    user_id = []
+    for user in raw_tweets['user']:
+        uid = user['id']
+        user_id.append(uid)
+    raw_tweets['userId'] = user_id
+    tweets = raw_tweets
+    tweets.rename(columns={'id':'tweetId', 'url':'tweetUrl'}, inplace=True)
+    return tweets
+
+def get_raw_tweets():
+    return pd.read_json('data-test.json', lines=True)
